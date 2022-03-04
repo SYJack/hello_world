a966df4511278e935946577ee36db3bcb31074f8
Jack <jacksu@kaishing-china.cn>
2022-03-04 17:52:32 +0800

    学习提交

--- src/main/java/com/jack/helloworld/HelloWorldApplication.java
+++ src/main/java/com/jack/helloworld/HelloWorldApplication.java
@@ -1,13 +1,17 @@
 package com.jack.helloworld;
 
+import com.jack.helloworld.config.DBConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+//@EnableConfigurationProperties({DBConfig.class})
 public class HelloWorldApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+        ConfigurableApplicationContext context =  SpringApplication.run(HelloWorldApplication.class, args);
+        DBConfig bean =  context.getBean(DBConfig.class);
+        System.out.println(bean);
     }
-
 }
---
+++ src/main/java/com/jack/helloworld/config/DBConfig.java
@@ -0,0 +1,14 @@
+package com.jack.helloworld.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "spring.datasource")
+@Data
+public class DBConfig {
+    private String url;
+    private String username;
+    private String password;
+}
